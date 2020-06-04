209358bc207b43dac79443503d13ae6dab6d39ac
Sebastiaan van der Tol <tol@astron.nl>
2020-06-04 08:34:06 +0200

    Fix python find in casacore recipe

--- recipes/casacore/build.cmake
+++ recipes/casacore/build.cmake
@@ -9,5 +9,7 @@ cmake_minimum_required(VERSION 3.0)
 # endif()
 
 set(Python3_ROOT_DIR $ENV{PYTHON_ROOT_DIR} CACHE PATH "Python root")
+set(BOOST_PYTHON3_LIBRARY_NAME python CACHE PATH "Name of boost python library to search for")
+set(PYTHON_EXECUTABLE "${Python3_ROOT_DIR}/bin/python3" CACHE FILEPATH "Python executable")
 
 include(${CGET_CMAKE_ORIGINAL_SOURCE_FILE})
